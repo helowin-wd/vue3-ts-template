60f2b8771450b6620696f1572350adbfdb95da03
wudong <wudong_wy@163.com>
2023-09-19 20:50:38 +0800

    test: 测试代码格式化效果

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,10 @@
 </template>
 
 <script setup lang="ts">
-for(let i=0;i<5;i++)
-{
+for (let i = 0; i < 5; i++) {
   console.log(i)
 }
+var aa = '测试代码格式化效果';
 </script>
 
-<style scoped lang="scss">
-
-</style>
+<style scoped lang="scss"></style>
